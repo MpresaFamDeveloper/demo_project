c644a221910ce360b81b91f811e0daabe84fb809
Advocate_Sandile <adv_sandile.ngcobo@outlook.com>
2022-07-21 12:32:32 -0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle kCustomText(
+        {double fontSize = 16.0,
+        Color color = Colors.black,
+        FontWeight fontWeight = FontWeight.normal}) =>
+    TextStyle(fontSize: fontSize, color: color, fontWeight: fontWeight);
